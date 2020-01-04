787cd7bfc8ae847922c40b7fcbb2497c51850888
André Tiago <andre.tiago.at@gmail.com>
2020-01-04 12:17:17 +0000

    1.2 em C

--- 1.2.c
+++ 1.2.c
@@ -1,8 +1,7 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
-
-void matMult(float **A, float **B, float **C, int N) {
+void matMult(int N, float A[N][N], float B[N][N], float C[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             C[i][j] = 0;
@@ -14,19 +13,16 @@ void matMult(float **A, float **B, float **C, int N) {
 }
 
 int main() {
-    int N = 512;
-    float **A = new float *[N], **B = new float *[N], **C = new float *[N];
+    int N = 3;
+
+    float A[N][N], B[N][N], C[N][N];
 
     for (int i = 0; i < N; ++i) {
-        A[i] = new float[N];
-        B[i] = new float[N];
-        C[i] = new float[N];
-
         for (int j = 0; j < N; ++j) {
             A[i][j] = ((float)rand()) / ((float)RAND_MAX);
             B[i][j] = 1;
         }
     }
 
-    matMult(A, B, C, N);
+    matMult(N, A, B, C);
 }
