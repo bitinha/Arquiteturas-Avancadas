e408e868127cbacfffd2b480ed08d55bc0b3f50b
bitinha <vitordagomes@outlook.com>
2020-01-07 12:09:21 +0000

    multiplicação de matrizes sem transposição

--- matMult-ijk-trans.c
+++ matMult-ijk-trans.c
@@ -2,14 +2,33 @@
 #include <stdlib.h>
 
 
+//https://en.wikipedia.org/wiki/In-place_matrix_transposition#Square_matrices
+void transpose(int N, float M[][N]){
+
+    float temp;
+
+    for (int i = 0; i < N-2; ++i)
+    {
+        for (int j = i+1; j < N-1; ++j)
+        {
+            temp = M[i][j];
+            M[i][j] = M[j][i];
+            M[j][i] = temp;
+        }
+    }
+
+}
 
 void matMult(int N, float A[][N], float B[][N], float C[][N]) {
+
+
+    transpose(N,B);
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             C[i][j] = 0;
             for (int k = 0; k < N; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A[i][k] * B[j][k];
             }
         }
     }
--- matMult-jki-trans.c
+++ matMult-jki-trans.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//https://en.wikipedia.org/wiki/In-place_matrix_transposition#Square_matrices
+void transpose(int N, float M[][N]){
+
+    float temp;
+
+    for (int i = 0; i < N-2; ++i)
+    {
+        for (int j = i+1; j < N-1; ++j)
+        {
+            temp = M[i][j];
+            M[i][j] = M[j][i];
+            M[j][i] = temp;
+        }
+    }
+
+}
 
 void matMult(int N, float A[][N], float B[][N], float C[][N]) {
 
+    transpose(N,A);
+    transpose(N,B);
+    // C também é transposta
 
     for (int j = 0; j < N; j++) {
 
         for (int i = 0; i < N; i++) {   //1ª iteração é necessário definir ter a matriz resultado a 0
-            C[i][j] = A[i][0] * B[0][j];
+            C[j][i] = A[0][i] * B[j][0];
         }
 
         for (int k = 1; k < N; k++) {
             for (int i = 0; i < N; i++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[j][i] += A[k][i] * B[j][k];
             }
         }
     }
